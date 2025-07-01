5d216e76952d462733b657f9a2b7f9465e6bc644
PriscilaO25 <priscilaolariaga@gmail.com>
2025-06-30 21:16:33 -0300

    Header y Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './pages/Home';
+import Footer from './Components/Footer'; 
 
-function App() {
- 
 
-  return (
-    <>
-      <div>
-       
-      </div>
-      
-    </>
-  )
-}
 
-export default App
+const App = () => (
+  <Router>
+    <Header />
+    <main className="container mt-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
+    <Footer />
+  </Router>
+);
+
+export default App;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,14 @@
 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Main = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Main
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
+
